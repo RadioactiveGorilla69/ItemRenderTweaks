476a9d444c947a4a60abd45703de3713011b8adc
RadioactiveGorilla69 <mindcraf.player@gmail.com>
2025-06-08 14:45:47 -0700

    Update EntityRenderMixin.java
    only display items from further if in affected item list

--- src/main/java/com/radioactivegorilla/mixin/EntityRenderMixin.java
+++ src/main/java/com/radioactivegorilla/mixin/EntityRenderMixin.java
@@ -1,8 +1,10 @@
 package com.radioactivegorilla.mixin;
 
+import com.radioactivegorilla.config.Config;
 import net.minecraft.client.render.Frustum;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.registry.Registries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,8 +15,13 @@ public class EntityRenderMixin {
     @Inject(method = "shouldRender", at = @At("HEAD"), cancellable = true)
     public void increaseRenderDistance(Entity entity, Frustum frustum, double x, double y, double z, CallbackInfoReturnable<Boolean> cir) {
         if (entity instanceof ItemEntity) {
-            //TRYING to show item as far away as possible, if it doesn't work then let me know.
-            cir.setReturnValue(true);
+            String itemIdString = Registries.ITEM.getId(((ItemEntity) entity).getStack().getItem()).toString();
+            String displayName = ((ItemEntity) entity).getStack().getName().getString();
+
+            if (Config.affectedItemsList.contains(itemIdString) || Config.affectedItemsList.contains(displayName)) {
+                //TRYING to show item as far away as possible, if it doesn't work then let me know.
+                cir.setReturnValue(true);
+            }
         }
     }
 }
